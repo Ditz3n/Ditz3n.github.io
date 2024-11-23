dacae506e14b6170aeac168173f8948215ea6cb5
Mads Villadsen <mvmads@gmail.com>
2024-11-23 17:23:01 +0100

    deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // This is the default value for Ditz3n.github.io
+  base: '/Ditz3n.github.io/frontend', // This is the default value for Ditz3n.github.io
 });
